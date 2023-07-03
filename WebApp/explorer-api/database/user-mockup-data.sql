97b939afa84ebe3001931721aae751e2c537f5b7
Jimmy Ulloa <umjim2021@gmail.com>
2023-07-03 08:05:29 -0600

    New files

--- WebApp/explorer-api/database/user-mockup-data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO "user" ("email", "username", "name", "surname", "password", "isAdmin") VALUES ('test@stalicla.com', 'test', 'admin', 'root', 'passowrd', true);
--- WebApp/explorer-api/src/utils/utils.ts
+++
@@ -1,3 +0,0 @@
-export function formatDateNumber(number: string | number): string {
-    return ('0' + number).slice(-2);
-}
